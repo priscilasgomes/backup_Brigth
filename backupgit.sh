dc1e66e81cb7ab945908f644b29c188453e637d0
root <root@VM>
2026-04-30 16:07:28 -0300

    Backup automatico Thu Apr 30 04:07:28 PM -03 2026 backup_2026-04-30_16-07-28.tar.gz

---
+++ backupgit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+DATA=$(date +%Y-%m-%d_%H-%M-%S)
+ORIGEM="/home/setor"
+DESTINO="/home/sec4you/Scripts/Projeto/Backup/backup_Brigth"
+NOME_ARQUIVO="backup_$DATA.tar.gz"
+LOG_FILE="backup.log"
+
+
+
+tar -czf "$DESTINO/$NOME_ARQUIVO" "$ORIGEM"
+
+
+cd $DESTINO
+
+git add .
+git commit -m "Backup automatico $(date) $NOME_ARQUIVO" 
+git push origin main
+
+echo "Backup $NOME_ARQUIVO enviado com sucesso"
